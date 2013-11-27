fc2e976994298dc1150ed8fc13d240c569178998
Sébastien Vaucher <seb.vaucher@gmail.com>
2013-11-27 14:52:19 +0100

    Syntaxical analysis

--- parser.py
+++ parser.py
@@ -14,7 +14,7 @@ precedence = (
 	( 'left' , 'ADD_OP' ),
 	( 'left' , 'MUL_OP' ),
 	( 'right' , 'UMINUS' ),
-)  
+)
 
 
 def p_program_statement(p):
@@ -22,37 +22,39 @@ def p_program_statement(p):
 	p[0] = AST.ProgramNode( p[1] )
   
 def p_program_recursive(p):
-	"""program : statement ';' program"""
-	p[0] = AST.ProgramNode( [p[1]] + p[3].children )
+	"""program : statement program"""
+	p[0] = AST.ProgramNode( [p[1]] + p[2].children )
 	
 def p_structure(p):
-	"""structure : WHILE expression '{' program '}' """
+	"""structure : while expression '{' program '}' """
 	p[0] = AST.WhileNode( [ p[2], p[4] ] )
+	
+def p_condition(p):
+	"""condition : if expression '{' program '}' """
+	p[0] = AST.IfNode( [ p[2], p[4] ] )
 
 def p_statement(p):
-	"""statement : assignation
-				| structure"""
+	"""statement : assignation ';'
+				| structure
+				| condition"""
 	p[0] = p[1]
-
-def p_statement_stream(p):
-	"""statement : IMAGE STREAM_OUT expression"""
-	# TODO : build an image here ?
 
 def p_assignation(p):
 	"""assignation : IDENTIFIER '=' expression"""
 	p[0] = AST.AssignNode([AST.TokenNode(p[1]),p[3]])
+	
+def p_stream_out(p):
+	"""assignation : IMAGE STREAM_OUT expression"""
+	p[0] = AST.OutNode([AST.TokenNode(p[1]), p[3]])
   
 def p_expression_identifier(p):
 	"""expression : IDENTIFIER"""
 	p[0] = AST.TokenNode(p[1])
   
-def p_expression_num(p):
-	"""expression : NUMBER"""
+def p_expression_numcolor(p):
+	"""expression : NUMBER
+					| COLOR"""
 	p[0] = AST.TokenNode(p[1])
-	
-def p_expression_color(p):
-	"""expression : COLOR"""
-	# TODO : assignation of a color structure
   
 def p_expression_uminus(p):
 	"""expression : ADD_OP expression  %prec UMINUS"""
@@ -61,6 +63,10 @@ def p_expression_uminus(p):
 def p_expression_brac(p):
 	"""expression : '(' expression ')'"""
 	p[0] = p[2]
+	
+def p_expression_comp(p):
+	"""expression : expression CMP_OP expression"""
+	p[0] = AST.CompNode(p[2],[p[1],p[3]])
 
 def p_expression_op(p):
 	"""expression : expression ADD_OP expression
@@ -90,7 +96,7 @@ if __name__ == "__main__":
 		print ("No file found, input is taken as program")
 		prog = sys.argv[1]
 	# result = yacc.parse(prog, debug=1)
-	result = yacc.parse(prog)
+	result = yacc.parse(prog, debug=1)
 	print("%s \n\n %s" % (prog, result))
 	graph = result.makegraphicaltree()
 	name = os.path.splitext(sys.argv[1])[0]+'-ast.pdf'
